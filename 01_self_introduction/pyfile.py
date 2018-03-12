7bbcc71195c49f7aaf14bf0ba8933377cc76e2dc
Dai Ando <dai.ando@rakuten.com>
2018-03-12 15:52:37 +0900

    self introduction

---
+++ 01_self_introduction/pyfile.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3.6
+
+first_name = input('first name : ')
+last_name = input('last name : ')
+age = input('age : ')
+birth_date = input('birth date : ')
+print(f"My name is {first_name} {last_name}.\nI was born on {birth_date}, and I'm {age} years old.")
